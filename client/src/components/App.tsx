12fb242e69db9e5479e76df2a2ba7f5605a0b502
Jarosław Nieściór <jaroslaw.niescior@gmail.com>
2021-07-17 19:49:29 +0200

    style commponents

---
+++ client/src/components/App.tsx
@@ -0,0 +1,39 @@
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import memories from '../images/memories.png';
+import Form from './Form';
+import Posts from './Posts';
+import useStyles from './style';
+
+const App = () => {
+  const classes = useStyles();
+
+  return (
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+          Memories
+        </Typography>
+        <img className={classes.image} src={memories} alt="memories"></img>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
+};
+
+export default App;
